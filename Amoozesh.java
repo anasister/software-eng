81a2d60f32a449b0157149ed7b4f471f0a19a484
atefeh <atefeh.ir72@yahoo.com>
2014-03-14 04:33:35 -0700

    modify

--- Amoozesh.java
+++ Amoozesh.java
@@ -6,7 +6,7 @@ public class Amoozesh {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello");
+		System.out.println("hello mr irani");
 	}
 
 }
